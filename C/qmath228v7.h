d8c5155e1a73182ed7a519e2f2c6a338557d6889
David S. Knight <davids.knight42@gmail.com>
2017-01-25 16:17:25 -0600

    Update qmath228v7.h

--- C/qmath228v7.h
+++ C/qmath228v7.h
@@ -163,10 +163,6 @@ int i;
 integer t;
 integer t1[220] = {0};
 integer t2[220] = {0};
-/*
-integer t1[m] = {0};
-integer t2[m] = {0};
-*/
 
 for (i=0;i<m2;i++)
 	{
@@ -955,12 +951,7 @@ void det5b (int c1, int c2, int c3, int c4, int m2, int e, int e1,
 int t;
 int t1,t2,t3,t4;
 
-unsigned int i;
-/*
-unsigned int d;
-
-d = d1+1;
-*/
+unsigned int i
 
 t1 = c1;
 t2 = c2;
