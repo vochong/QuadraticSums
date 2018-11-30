829b75fe4ae6070303021cf6105dda4ddb0c3f9a
David S. Knight <davids.knight42@gmail.com>
2018-11-29 19:56:32 -0600

    Update series125v3.py

--- series/series125v3.py
+++ series/series125v3.py
@@ -2425,25 +2425,17 @@ d3 = [d[0] , d[1]]
 h3 = k.series25(-l2,l2,d3,d1*d1)
 print("\n \n h3  = ",h3)
 
-
 m4,b4 = init81(g,l2,d,d1,h1,h2,h3,p)
 
 b3 = init2(157)
 for i in range(149):
     b3[i] = multcnvl2(m4[i],k125,p)
 
-
 print("\n \n b3 = ")
 display149(b3)
 
-# print("\n \n b3 = ")
-# display149v2(b3)
-
 print("\n \n b4 =")
 display125(b4)
-
-# print("\n \n b4 =")
-# display125v2(b4)
 
 m = m4
 v1 = init2(125)
@@ -2480,5 +2472,4 @@ h1[0] = (t1+(p-1)*b3[125])%p
 h1[1] = (t2+(p-1)*b3[126])%p
 h1[2] = (t3+(p-1)*b3[127])%p
 h1[3] = (t4+(p-1)*b3[128])%p
-print("\n \n h1 = ",h1)
-        
+print("\n \n h1 = ",h1)        
